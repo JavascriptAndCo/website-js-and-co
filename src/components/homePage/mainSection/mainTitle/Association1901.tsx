5b758439f305cd0ee839845b1f10889a18971914
Thomas <barrialthomas@gmail.com>
2021-09-01 14:44:50 +0200

    rm text shadow on main title

--- src/components/homePage/mainSection/mainTitle/Association1901.tsx
+++ src/components/homePage/mainSection/mainTitle/Association1901.tsx
@@ -1,17 +1,10 @@
 import React from "react";
 
-function Association1901({
-    textShadow,
-    animation,
-}: {
-    textShadow: string;
-    animation: string;
-}): JSX.Element {
+function Association1901({ animation }: { animation: string }): JSX.Element {
     return (
         <div className="font-titilumWeb flex justify-center mt-32">
             <span
                 className={`tracking-widest lg:text-xl text-md animate-fade ${animation}`}
-                style={{ textShadow: textShadow }}
             >
                 ASSOCIATION 1901
             </span>
--- src/components/homePage/mainSection/mainTitle/MainTitle.tsx
+++ src/components/homePage/mainSection/mainTitle/MainTitle.tsx
@@ -6,7 +6,7 @@ import Formation from "./Formation";
 import Button from "./ButtonsSec1";
 import Association1901 from "./Association1901";
 
-function MainTitle({ textShadow }: { textShadow: string }): JSX.Element {
+function MainTitle(): JSX.Element {
     const [animation, setAnimation] = useState("");
     const [animation2, setAnimation2] = useState("");
     const [animation3, setAnimation3] = useState("");
@@ -23,22 +23,19 @@ function MainTitle({ textShadow }: { textShadow: string }): JSX.Element {
     }, []);
     return (
         <div className="h-max-10/12 mt-32">
-            <JSandCo textShadow={textShadow} />
-            <Association1901 textShadow={textShadow} animation={animation} />
+            <JSandCo />
+            <Association1901 animation={animation} />
             <Vulgarisation
-                textShadow={textShadow}
                 animation={animation}
                 animation2={animation2}
                 animation3={animation3}
             />
             <Diffusion
-                textShadow={textShadow}
                 animation={animation}
                 animation2={animation2}
                 animation3={animation3}
             />
             <Formation
-                textShadow={textShadow}
                 animation={animation}
                 animation2={animation2}
                 animation3={animation3}
